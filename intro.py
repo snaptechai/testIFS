4b381e8b4248ae1cbc29e4c46c6c657bf1756d30
snaptechai <info@snaptechai.com>
2025-06-10 14:58:32 +0530

    Committed greet adl

--- intro.py
+++ intro.py
@@ -1,4 +1,4 @@
 def greetings_adl(company):
-    print(f"Hello {company}, This is a sample docker image created for the pipeline, now its in Docker HUB")
+    print(f"Hello {company}, This is a sample docker image created for the pipeline, now its in Docker hub")
     
 greetings_adl("ADL")
