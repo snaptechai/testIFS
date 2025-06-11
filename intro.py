d6fb1abebfc0ddc493abb17d0a1aeb52f68d745a
snaptechai <info@snaptechai.com>
2025-06-11 08:38:27 +0530

    small change

--- intro.py
+++ intro.py
@@ -1,4 +1,4 @@
 def greetings_adl(company):
-    print(f"Hello {company}, This is a sample docker image created for the pipeline, now its in Docker hub")
+    print(f"Hello {company}, This is a sample docker image created for the pipeline, now its in Dockerhub")
     
 greetings_adl("ADL")
