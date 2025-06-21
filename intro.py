13457045982a9a305a97d869e49ee65ff5d29129
snaptechai <info@snaptechai.com>
2025-06-21 18:44:21 +0530

    commited code

--- intro.py
+++ intro.py
@@ -1,4 +1,4 @@
 def greetings_adl(company):
-    print(f"Hello {company}, This is a sample docker image created for the pipeline!!")
+    print(f"Hello {company}, This is a sample new docker image created for the pipeline!!")
     
 greetings_adl("ADL")
